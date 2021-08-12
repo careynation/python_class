e0dc7ba98c4915ab79553ccb33edaa63f7577bee
Carey Nation <carey.nation@turner.com>
2021-08-12 09:59:35 -0400

    Initial checkin

---
+++ Chapter2/exercise3.py
@@ -0,0 +1,15 @@
+# @author Carey Nation
+# @title Chapter 2, Exercise 2
+# @description Calculate acreage
+
+sq_ft_per_acre = 43560
+
+sq_ft_s = input("Enter your square feet: ")
+
+try:
+    sq_ft = float(sq_ft_s)
+    acres = sq_ft / sq_ft_per_acre
+
+    print("You have %.2f acres of land" % acres)
+except:
+    print(sq_ft_s + " is not a valid number, please try again")
