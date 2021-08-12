72044b181b73392ee8f9705d474954af319b1564
Carey Nation <carey.nation@turner.com>
2021-08-12 09:18:38 -0400

    Initial checking

---
+++ Chapter2/exercise1.py
@@ -0,0 +1,10 @@
+# @author Carey Nation
+# @title Chapter 2, Exercise 1
+# @description Print your name and address
+
+print("Carey Nation")
+print("123 Nowhere Lane")
+print("Nope, GA")
+print("30303")
+print("404-555-1212")
+print("Computer Science")
