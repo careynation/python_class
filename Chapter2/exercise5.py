6ea07f1ca36cdd89f687e25398ea248887645314
Carey Nation <carey.nation@turner.com>
2021-08-12 10:32:34 -0400

    Initial checkin

---
+++ Chapter2/exercise5.py
@@ -0,0 +1,12 @@
+# @author Carey Nation
+# @title Chapter 2, Exercise 5
+# @description Calculate distance traveled
+
+
+def distance(time):
+    return time * 70
+
+
+print("Distance in six hours: %.2f miles" % distance(6))
+print("Distance in ten hours: %.2f miles" % distance(10))
+print("Distance in fifteen hours: %.2f miles" % distance(15))
